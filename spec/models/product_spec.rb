b07ecf1a14626655fcc9c97183513108162d0fa1
dextergb <dextergb@rogers.com>
2021-07-27 20:44:25 +0000

    Fix bug

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Product, type: :model do
     it 'will not save if category is nil' do
       @category = Category.new({name: "test"})
       @product = @category.products.new(name: 'hat', price: 10, quantity: 50, category: nil)
-      expect(@test_product).to_not be_valid
+      expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
