a42ad0d12033f4804ab5949c9085b57e39ae464c
dextergb <dextergb@rogers.com>
2021-07-28 06:08:14 +0000

    test that users can click the 'Add to Cart' button for a product on the home page and in doing so their cart increases by one

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Add to cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "User can click on the 'Add to Cart' button for a product on the home page and in doing so their cart increases by one" do
+    # ACT
+    visit root_path
+    within first('.product') do
+      click_button 'Add'
+    end
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_text('My Cart (1)')
+    save_screenshot('add_cart.png')
+  end
+end
